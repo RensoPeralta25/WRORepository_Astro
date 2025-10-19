c12519703fac983a37cc5f9a98d9eaf596bc1dd8
Renso I. Peralta U. <ripu0001@ce.pucmm.edu.do>
2025-10-19 10:07:38 -0400

    Create botton.py

---
+++ src/botton.py
@@ -0,0 +1,18 @@
+from robot_hat import Pin
+import time
+import os
+
+button = Pin("SW", Pin.IN, Pin.PULL_UP)   # Botón USR (GPIO25)
+led = Pin(26, Pin.OUT)                    # LED (GPIO26)
+
+# Ruta del archivo que quieres ejecutar
+script_path = "/home/admin/Documents/pruebaAbierta1.py"
+led.on()
+while True:
+    if button.value() == 1:  # Presionado
+        led.on()
+        os.system(f"python3 {script_path}")  # Ejecuta el otro script
+        time.sleep(0.5)  # Evita que se repita por rebote
+    else:
+        led.off()
+    time.sleep(0.1)
